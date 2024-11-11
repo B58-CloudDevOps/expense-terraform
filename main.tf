49056cc3fecc3c40a9789024de2e08837eed00c9
CodingManoj <CodingManoj@gmail.com>
2024-11-10 22:07:22 -0500

    Kubernetes AutoPush

--- main.tf
+++ main.tf
@@ -68,14 +68,16 @@ module "rds" {
   source     = "git::https://github.com/B58-CloudDevOps/tf-module-rds.git"
   for_each   = var.rds
 
-  engine          = each.value["engine"]
-  engine_verison  = each.value["engine_version"]
+  engine = each.value["engine"]
+
   env             = var.env
   family          = each.value["family"]
   instance_class  = each.value["instance_class"]
   subnet_ids      = module.vpc["main"].db_subnet_ids
   eks_subnet_cidr = module.vpc["main"].eks_subnet_cidr
   vpc_id          = module.vpc["main"].vpc_id
+
+  engine_verison = each.value["engine_version"]
 
 }
 
